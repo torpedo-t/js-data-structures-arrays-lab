3574e946893d6b2bac59b5c7eb0d21c4a7bf9d64
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-07-27 12:47:28 -0500

    Done.

--- index.js
+++ index.js
@@ -2,5 +2,33 @@
 const drivers = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendDriver(name) {
-    return [...drivers, 'Ralph'];
+    return drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+    return drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver(name) {
+    return drivers.pop(name);
+}
+
+function destructivelyRemoveFirstDriver(name) {
+    return drivers.shift(name);
+}
+
+function appendDriver(name) {
+    return [...drivers, name];
+}
+
+function prependDriver(name) {
+    return [name, ...drivers];
+}
+
+function removeLastDriver() {
+    return drivers.slice(0, drivers.length - 1);
+}
+
+function removeFirstDriver() {
+    return drivers.slice(1);
 }
